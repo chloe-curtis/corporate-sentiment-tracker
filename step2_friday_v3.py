3475cd90c6bee388cc10f8e53005dfa65a6277fd
Josh Hartshorne <joshah@hotmail.co.uk>
2025-06-06 15:25:57 +0100

    splits quarter to year quarter

---
+++ step2_friday_v3.py
@@ -0,0 +1,22 @@
+base_index = pd.merge(historical_index, ticker_conversion, on = "ticker", how = "left")
+### JOIN CAUSED NAS - AS TICKER CONVERSION DIDNT HAVE COMPANY NAME AND CIK
+### REMOVE THE NAS FROM THE JOIN
+x = base_index.dropna()
+## CREATE DF WITH THE NAS
+y = base_index[base_index.isnull().any(axis=1)]
+## DROP THE COLUMNS WE DONT NEED FROM NA DF
+g = y[['quarter','ticker']]
+## CSV HAS THE DATA WE NEED
+missing_company = pd.read_csv('missing_companies.csv')
+### MERGE BACK TO HAVE THE CIX AND COMPANY NAME 
+z = pd.merge(g, missing_company, on = "ticker", how = "left")
+### PAD THE CIK NUMBERS WITH ZEROS - 10 CHARCTERS
+z['cik'] = z['cik'].astype(str).str.zfill(10)
+## CONCAT BOTH DFS
+df = pd.concat([x, z], axis=0, ignore_index=True)
+# Split the 'quarter' column into two parts
+df[['quarter', 'year']] = df['quarter'].str.split('-', expand=True)
+
+# Convert 2-digit year to 4-digit
+df['year'] = df['year'].apply(lambda x: '20' + x if int(x) < 50 else '19' + x)
+df['year'] = df['year'].astype(int)
