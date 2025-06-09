0be01e4db35fc067c93e7921321cbcafe1580235
emre yildiz <emreyildizuk@gmail.com>
2025-06-09 16:33:24 +0100

    getting sentiment for text

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from model import get_sentiment_stats_from_text
+from model import TEST_MDA
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Backend is working!"}
+
+@app.get("/get_sentiment")
+def get_sentiment(text = TEST_MDA):
+
+    print("getting sentiment for text:", text)
+    #use harsh's code to get sentiment from text
+
+
+    return {"sentiment": "positive"}
